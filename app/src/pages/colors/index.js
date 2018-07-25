0a1ea65f8291f542c9f3d599745b33f22bca9e59
Josh Hethcox <josh.hethcox@gmail>
2018-07-25 10:58:02 -0400

    stage 4 complete, but colors dont show, moving anyways

---
+++ app/src/pages/colors/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+//import fetch from "isomorphic-fetch";
+import { map } from "ramda";
+import { connect } from "react-redux";
+
+const li = color => {
+  return (
+    <li key={color.id} style={{ color: color.value }}>
+      {color.name}
+    </li>
+  );
+};
+
+const Colors = props => {
+  return (
+    <div>
+      <h1>Colors</h1>
+      <ul>{map(li, props.colors)}</ul>
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { colors: state.colors };
+};
+
+const connector = connect(mapStateToProps);
+
+export default connector(Colors);
